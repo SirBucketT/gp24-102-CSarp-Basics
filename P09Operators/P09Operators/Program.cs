20f8995c1ff465e44914440d58f11bccb3f51f93
SirBucketT <tkpriva@icloud.com>
2024-09-17 14:30:28 +0200

    slight modifications

---
+++ P09Operators/P09Operators/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string Question = ("Give me a number of seconds");
+Console.WriteLine(Question);
+
+string secondValue = Console.ReadLine();
+float floatValue = float.Parse(secondValue);
+int i = (int) floatValue;
+
