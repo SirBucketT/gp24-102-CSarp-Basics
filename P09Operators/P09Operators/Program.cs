d8113f9ecbbc3aa278e5c095f6d48c447102fb6f
SirBucketT <tkpriva@icloud.com>
2024-09-20 21:07:11 +0200

    part 9 new features a km/h to m/s converter

--- P09Operators/P09Operators/Program.cs
+++ P09Operators/P09Operators/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Data;
+using System;
 
 string Question = ("Give me a number of seconds");
 Console.WriteLine(Question);
@@ -8,12 +9,14 @@ Console.WriteLine(Question);
 string input = Console.ReadLine();
 int seconds = int.Parse(input);
 
+//calculations of each time unit into seconds
 int numbersOfDays = seconds / (24 * 3600);
 int remainingSeconds = seconds % (24 * 3600);
 int numberOfHours = remainingSeconds / 3600;
 remainingSeconds %= 3600;
 int numbersOfMinutes = remainingSeconds / 60;
 int numberOfSeconds = remainingSeconds % 60;
+
 
 Console.WriteLine("Number of days" + numbersOfDays);
 Console.WriteLine("Number of hours" + numberOfHours);
@@ -34,13 +37,17 @@ if (SpeedConversion == "Q")
     Console.Clear();
 }
 
+//Moves on to 2nd part of assignment.
+
 
 Console.WriteLine("P09_01SpeedConverter");
 Console.WriteLine("Give me a input of KM/h and I'll convert it into m/s.");
-string kmInput = Console.ReadLine();
-int km = int.Parse(kmInput);
+string kmhInput = Console.ReadLine();
+double kmh = double.Parse(kmhInput);
 
-int meters = km / 1000;
+//converts x km/h by 1000 meters and then devides that by 3600 to get the resulting seconds. Note to self, this took way too long to figure out...
 
-Console.WriteLine(meters);
+double ms = kmh * 1000 / 3600;
+
+Console.WriteLine($"{kmh} converted in m/s is {ms}/s");
 
--- P09Operators/P09Operators/obj/Debug/net8.0/P09Operators.AssemblyInfo.cs
+++ P09Operators/P09Operators/obj/Debug/net8.0/P09Operators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P09Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf7541f2ec3a3b57f16325ed5521dc28eaa4a48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c108e93da5c569ed57e9ff54507664a149bf38")]
 [assembly: System.Reflection.AssemblyProductAttribute("P09Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P09Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
