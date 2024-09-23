f4eca6f2f075b74adae4e9fe29d65d071aeb7351
SirBucketT <tkpriva@icloud.com>
2024-09-23 09:55:23 +0200

    part 10 of assignment complete

---
+++ P10Boolean/P10Boolean/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What's your age?");
+int age = int.Parse(Console.ReadLine());
+
+bool isTeenager = false;
+bool isChild = false;
+bool isAdult  = false;
+
+if (age <= 12) {
+    isChild = true;
+    Console.WriteLine("You are but a mere child");
+} if (age > 13 && age < 19)
+{
+    isTeenager = true;
+    Console.WriteLine("You are but a mere teenager");
+}
+if (age >= 19)
+{
+     isAdult = true;
+    Console.WriteLine("You are a adult");
+    
+} 
