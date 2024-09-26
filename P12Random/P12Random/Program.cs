95fdc91cb1c7b0e2f068084934c22b8989369354
SirBucketT <tkpriva@icloud.com>
2024-09-26 14:44:37 +0200

    tweaks to part 12 text

--- P12Random/P12Random/Program.cs
+++ P12Random/P12Random/Program.cs
@@ -145,138 +145,138 @@ Console.WriteLine("Type 'fun' to see what loot you get");
 string funboxes = Console.ReadLine();
     if (funboxes == "fun" || funboxes == "FUN" || funboxes == "fUn" || funboxes == "Fun")
     {
-        switch (itemGenerator)
+            switch (itemGenerator) 
+            {
+                //item 1 drop
+                case >= 0 and <= 4:
+                    itemDropped = pants;
+                    break;
+                case 5:
+                    itemDropped = superRareSword;
+                    break;
+                case >= 6 and <= 19:
+                    itemDropped = rustySword;
+                    break;
+                case >= 20 and <= 29:
+                    itemDropped = cloakOfInvisibility;
+                    break;
+                case >= 30 and <= 39:
+                    itemDropped = wankySword;
+                    break;
+                case >= 40 and <= 41:
+                    itemDropped = excaliburSword;
+                    break;
+                case >= 42 and <= 49:
+                    itemDropped = hat;
+                    break;
+                case >= 50 and <= 69:
+                    itemDropped = plateArmor;
+                    break;
+                case >= 70 and <= 85:
+                    itemDropped = boot;
+                    break;
+                default:
+                    itemDropped = chestPlate;
+                    break;
+            }
+
+            switch (itemGenerator2)
+            {
+                //item 2 drop
+                case >= 0 and <= 4:
+                    itemDropped2 = pants;
+                    break;
+                case 5:
+                    itemDropped2 = superRareSword;
+                    break;
+                case >= 6 and <= 19:
+                    itemDropped2 = rustySword;
+                    break;
+                case >= 20 and <= 29:
+                    itemDropped2 = cloakOfInvisibility;
+                    break;
+                case >= 30 and <= 39:
+                    itemDropped2 = wankySword;
+                    break;
+                case >= 40 and <= 41:
+                    itemDropped2 = excaliburSword;
+                    break;
+                case >= 42 and <= 49:
+                    itemDropped2 = hat;
+                    break;
+                case >= 50 and <= 69:
+                    itemDropped2 = plateArmor;
+                    break;
+                case >= 70 and <= 85:
+                    itemDropped2 = boot;
+                    break;
+                default:
+                    itemDropped2 = chestPlate;
+                    break;
+            }
+
+            switch (itemGenerator3)
+            {
+                //item 3 drop
+                case >= 0 and <= 4:
+                    itemDropped3 = pants;
+                    break;
+                case 5:
+                    itemDropped3 = superRareSword;
+                    break;
+                case >= 6 and <= 19:
+                    itemDropped3 = rustySword;
+                    break;
+                case >= 20 and <= 29:
+                    itemDropped3 = cloakOfInvisibility;
+                    break;
+                case >= 30 and <= 39:
+                    itemDropped3 = wankySword;
+                    break;
+                case >= 40 and <= 41:
+                    itemDropped3 = excaliburSword;
+                    break;
+                case >= 42 and <= 49:
+                    itemDropped3 = hat;
+                    break;
+                case >= 50 and <= 69:
+                    itemDropped3 = plateArmor;
+                    break;
+                case >= 70 and <= 85:
+                    itemDropped3 = boot;
+                    break;
+                default:
+                    itemDropped3 = chestPlate;
+                    break;
+            }
+
+        Console.WriteLine($"Item drops \n {itemDropped} \n {itemDropped2} \n {itemDropped3} \n added to inventory");
+        Console.WriteLine("Type 'Equip' to equip the items or 'Drop' to remove the items from the inventory.");
+        string equippedItem = Console.ReadLine();
+
+        if (equippedItem == "Equip" ||  equippedItem == "equip")
         {
-            //item 1 drop
-            case >= 0 and <= 4:
-                itemDropped = pants;
-                break;
-            case 5:
-                itemDropped = superRareSword;
-                break;
-            case >= 6 and <= 19:
-                itemDropped = rustySword;
-                break;
-            case >= 20 and <= 29:
-                itemDropped = cloakOfInvisibility;
-                break;
-            case >= 30 and <= 39:
-                itemDropped = wankySword;
-                break;
-            case >= 40 and <= 41:
-                itemDropped = excaliburSword;
-                break;
-            case >= 42 and <= 49:
-                itemDropped = hat;
-                break;
-            case >= 50 and <= 69:
-                itemDropped = plateArmor;
-                break;
-            case >= 70 and <= 85:
-                itemDropped = boot;
-                break;
-            default:
-                itemDropped = chestPlate;
-                break;
+            Console.WriteLine($"Charecter equips" + 
+                              $"\n {itemDropped}" + 
+                              $"\n {itemDropped2} " + 
+                              $"\n {itemDropped3}");
+        
         }
-
-        switch (itemGenerator2)
+        else
         {
-            //item 2 drop
-            case >= 0 and <= 4:
-                itemDropped2 = pants;
-                break;
-            case 5:
-                itemDropped2 = superRareSword;
-                break;
-            case >= 6 and <= 19:
-                itemDropped2 = rustySword;
-                break;
-            case >= 20 and <= 29:
-                itemDropped2 = cloakOfInvisibility;
-                break;
-            case >= 30 and <= 39:
-                itemDropped2 = wankySword;
-                break;
-            case >= 40 and <= 41:
-                itemDropped2 = excaliburSword;
-                break;
-            case >= 42 and <= 49:
-                itemDropped2 = hat;
-                break;
-            case >= 50 and <= 69:
-                itemDropped2 = plateArmor;
-                break;
-            case >= 70 and <= 85:
-                itemDropped2 = boot;
-                break;
-            default:
-                itemDropped2 = chestPlate;
-                break;
+            Console.WriteLine($"Character dropped" + 
+                              $"\n {itemDropped}" + 
+                              $"\n {itemDropped2} " + 
+                              $"\n {itemDropped3}");
         }
-
-        switch (itemGenerator3)
-        {
-            //item 3 drop
-            case >= 0 and <= 4:
-                itemDropped3 = pants;
-                break;
-            case 5:
-                itemDropped3 = superRareSword;
-                break;
-            case >= 6 and <= 19:
-                itemDropped3 = rustySword;
-                break;
-            case >= 20 and <= 29:
-                itemDropped3 = cloakOfInvisibility;
-                break;
-            case >= 30 and <= 39:
-                itemDropped3 = wankySword;
-                break;
-            case >= 40 and <= 41:
-                itemDropped3 = excaliburSword;
-                break;
-            case >= 42 and <= 49:
-                itemDropped3 = hat;
-                break;
-            case >= 50 and <= 69:
-                itemDropped3 = plateArmor;
-                break;
-            case >= 70 and <= 85:
-                itemDropped3 = boot;
-                break;
-            default:
-                itemDropped3 = chestPlate;
-                break;
-        }
-
-    Console.WriteLine($"Item drops \n {itemDropped} \n {itemDropped2} \n {itemDropped3} \n added to inventory");
-    Console.WriteLine("Type 'Equip' to equip the items or 'Drop' to remove the items from the inventory.");
-    string equippedItem = Console.ReadLine();
-
-    if (equippedItem == "Equip" ||  equippedItem == "equip")
-    {
-        Console.WriteLine($"Charecter equips" + 
-                          $"\n {itemDropped}" + 
-                          $"\n {itemDropped2} " + 
-                          $"\n {itemDropped3}");
-    
-    }
-    else
-    {
-        Console.WriteLine($"Character dropped" + 
-                          $"\n {itemDropped}" + 
-                          $"\n {itemDropped2} " + 
-                          $"\n {itemDropped3}");
-    }
     }
 
 Console.WriteLine("press f and enter to wipe console and proceed to next step");
 string wiper4 = Console.ReadLine();
-if (wiper4 == "f" || wiper4 == "F")
-{
-    Console.Clear();
-}
+    if (wiper4 == "f" || wiper4 == "F")
+    {
+        Console.Clear();
+    }
 
 
 
